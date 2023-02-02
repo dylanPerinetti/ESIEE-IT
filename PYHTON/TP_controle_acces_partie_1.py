f68a224ae382e81a21e1e94db6eaa09bb41c03bf
Dylan Perinetti <101599798+dylanPerinetti@users.noreply.github.com>
2023-02-02 17:56:45 +0100

    Update TP_controle_acces_partie_1.py

--- PYHTON/TP_controle_acces_partie_1.py
+++ PYHTON/TP_controle_acces_partie_1.py
@@ -1,28 +1,37 @@
 #Coding : utf-8
-#
-#Prenom : Dylan
-#Nom    : PERINETTI
-#Date   : Mer 2er fèvr.
-#heure  : 00:32
-#Code   : Controle d'acces SOLO 1.2
-#ficname: controlAcc2.py
-#
+#   Prenom : Dylan
+#   Nom    : PERINETTI
+#   Date   : Mer 2er fèvr.
+#   heure  : 17:55
+#   Code   : Controle d'acces Securiser par sha512 2
+#   ficname: controlAcc2.py
 
-#Import des bibliotèque nécesaire
+#Import des fonction dans les bibliotèques 
 from datetime import datetime
+from hashlib import sha512
+import getpass
 
-message = {False: "Non autorisé", True: "Autorisé"}
- 
-#N° du Badge (n° qui sera normalement récupéré grâce au RFID)
-#Saisie utilisateur ID et MDP
-user_name  = input("\nUser name :")
-user_password = input("\nPassword  :")
 
 #ouvre tous les fichier d'extention .log et .data dans le repertoire courrant 
 alllog     = open("all.log", "a")
 autorlog   = open("Acces_autor.log", "a")
 faillog    = open("acces_interd.log", "a")
 autorliste = open(".data", "r")
+message = {False: "Non autorisé", True: "Autorisé"}
+
+
+#Saisie utilisateur ID et MDP à la place du n° du Badge qui sera normalement récupéré grâce au RFID
+user_name  = input("\nUser name :")
+user_password = getpass.getpass("\nPassword  :")
+cpy_user_pass = user_password
+
+#encode() pour géré les caractere utf-8
+user_password = user_password.encode()
+
+
+#sha512() hash le mdp
+user_password = str(sha512(user_password).hexdigest())
+
 
 #Boucle pour cherher une occurence de la concaténation de user;mdp dans le fichier cacher .data
 for ligne in autorliste:
@@ -32,15 +41,18 @@ for ligne in autorliste:
     else:
         autorisation = False
 
+
 #Afficher sur a l'écrant si l'on est autorisaté ou non!
 print(message[autorisation])
 
+
 if autorisation == True:
-    autorlog.write("\nACCES AUTORISER: "+str(datetime.now())+" USER: "+user_name+"\tPASSWORD: (hash)"+str(hash(user_password)))
-    alllog.write("ACCES AUTORISER: "+str(datetime.now())+" USER: "+user_name+"\tPASSWORD: (hash)"+str(hash(user_password))+"\n")
+    autorlog.write("\nACCES AUTORISER: "+str(datetime.now())+" USER: "+user_name+"\tPASSWORD: $sha512"+user_password)
+    alllog.write("ACCES AUTORISER: "+str(datetime.now())+" USER: "+user_name+"\tPASSWORD: $sha512"+user_password+"\n")
 else :
-    faillog.write("\nACCES INTERDIT:  "+str(datetime.now())+' USER: '+user_name+"\tPASSWORD: "+user_password)
-    alllog.write("ACCES INTERDIT: "+str(datetime.now())+' USER: '+user_name+"\tPASSWORD: "+user_password+"\n")
+    faillog.write("\nACCES INTERDIT :  "+str(datetime.now())+' USER: '+user_name+"\tPASSWORD: "+cpy_user_pass)
+    alllog.write("ACCES INTERDIT :  "+str(datetime.now())+' USER: '+user_name+"\tPASSWORD: "+cpy_user_pass+"\n")
+
 
 #fermer tous les fichier ouver 
 autorlog.close()
